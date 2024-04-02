0fb5b962ed259634f6e89e5e920d4c7ecb065ec9
Daanooo <daankraaijeveld@icloud.com>
2024-04-02 18:14:57 +0200

    Initial commit

---
+++ cmd/alfetch/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Daanooo/airlabs-fetcher/internal/fetcher"
+)
+
+func main() {
+	fetcher.Fetch()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Daanooo/airlabs-fetcher
+
+go 1.22.1
